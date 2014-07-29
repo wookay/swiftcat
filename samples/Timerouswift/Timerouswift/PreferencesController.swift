4d93b850280ccbfd6894cf963bea8e3aabeeb4e0
WooKyoung Noh <wookay.noh@gmail.com>
2014-07-29 18:57:30 +0900

    beta4

--- samples/Timerouswift/Timerouswift/PreferencesController.swift
+++ samples/Timerouswift/Timerouswift/PreferencesController.swift
@@ -11,20 +11,20 @@ import Cocoa
 class PreferencesController : NSWindowController {
     var timerController : TimerController? = nil
 
-    @IBOutlet var firstTextField : NSTextField
-    @IBOutlet var secondTextField : NSTextField
-    @IBOutlet var volumeSlider : NSSlider
+    @IBOutlet var firstTextField : NSTextField?
+    @IBOutlet var secondTextField : NSTextField?
+    @IBOutlet var volumeSlider : NSSlider?
 
     @IBAction func firstTextFieldChanged(textField : NSTextField) {
         let minutes = textField.stringValue
         NSUserDefaults.standardUserDefaults().setInteger(minutes.toInt()!, forKey: "First")
-        timerController!.firstButton.title = "\(minutes)분"
+        timerController!.firstButton!.title = "\(minutes)분"
     }
 
     @IBAction func secondTextFieldChanged(textField : NSTextField) {
         let minutes = textField.stringValue
         NSUserDefaults.standardUserDefaults().setInteger(minutes.toInt()!, forKey: "Second")
-        timerController!.secondButton.title = "\(minutes)분"
+        timerController!.secondButton!.title = "\(minutes)분"
     }
 
     @IBAction func volumeSliderChanged(volumeSlider : NSSlider) {
@@ -34,11 +34,11 @@ class PreferencesController : NSWindowController {
     }
     
     override func windowDidLoad() {
-        firstTextField.integerValue = NSUserDefaults.standardUserDefaults().integerForKey("First")
-        secondTextField.integerValue = NSUserDefaults.standardUserDefaults().integerForKey("Second")
-        volumeSlider.floatValue = NSUserDefaults.standardUserDefaults().floatForKey("Volume")
-        volumeSlider.minValue = 0.0
-        volumeSlider.maxValue = 1.0
+        firstTextField!.integerValue = NSUserDefaults.standardUserDefaults().integerForKey("First")
+        secondTextField!.integerValue = NSUserDefaults.standardUserDefaults().integerForKey("Second")
+        volumeSlider!.floatValue = NSUserDefaults.standardUserDefaults().floatForKey("Volume")
+        volumeSlider!.minValue = 0.0
+        volumeSlider!.maxValue = 1.0
     }
     
 }
--- samples/Timerouswift/Timerouswift/TimerController.swift
+++ samples/Timerouswift/Timerouswift/TimerController.swift
@@ -9,15 +9,15 @@
 import Cocoa
 
 class TimerController: NSWindowController, TimerDelegate {
-    @IBOutlet var firstButton: NSButton
-    @IBOutlet var secondButton: NSButton
-    @IBOutlet var oneMinutesMoreButton : NSButton
-    @IBOutlet var oneSecondsMoreButton : NSButton
-    @IBOutlet var startButton: NSButton
-    @IBOutlet var stopButton: NSButton
-    @IBOutlet var pauseButton : NSButton
-    @IBOutlet var finishedAt : NSTextField
-    @IBOutlet var display: NSTextField
+    @IBOutlet var firstButton: NSButton?
+    @IBOutlet var secondButton: NSButton?
+    @IBOutlet var oneMinutesMoreButton : NSButton?
+    @IBOutlet var oneSecondsMoreButton : NSButton?
+    @IBOutlet var startButton: NSButton?
+    @IBOutlet var stopButton: NSButton?
+    @IBOutlet var pauseButton : NSButton?
+    @IBOutlet var finishedAt : NSTextField?
+    @IBOutlet var display: NSTextField?
     
     var timer : Timer = Timer()
     var formatter : NSDateFormatter = NSDateFormatter()
@@ -27,18 +27,18 @@ class TimerController: NSWindowController, TimerDelegate {
     }
     
     func TimerStopped() {
-        startButton.enabled = true
-        pauseButton.enabled = false
-        stopButton.enabled = false
+        startButton!.enabled = true
+        pauseButton!.enabled = false
+        stopButton!.enabled = false
     }
     
     func TimerBeep() {
-        display.backgroundColor = NSColor.redColor()
+        display!.backgroundColor = NSColor.redColor()
         NSApplication.sharedApplication().activateIgnoringOtherApps(true)
     }
     
     func displayInit() {
-        display.backgroundColor = NSColor.blackColor()
+        display!.backgroundColor = NSColor.blackColor()
     }
     
     func defaultAction() {
@@ -49,16 +49,16 @@ class TimerController: NSWindowController, TimerDelegate {
         let ti : Int = Int(timer.remain)
         let seconds : Int = ti % 60
         let minutes : Int = (ti / 60) % 60
-        display.stringValue = NSString(format:"%u:%02u", minutes, seconds)
-        finishedAt.stringValue = formatter.stringFromDate(NSDate(timeIntervalSinceNow: timer.remain))
+        display!.stringValue = NSString(format:"%u:%02u", minutes, seconds)
+        finishedAt!.stringValue = formatter.stringFromDate(NSDate(timeIntervalSinceNow: timer.remain))
     }
     
     override func windowDidLoad() {
         timer.delegate = self
-        firstButton.title = NSString(format: "%d분", NSUserDefaults.standardUserDefaults().integerForKey("First"))
-        secondButton.title = NSString(format: "%d분", NSUserDefaults.standardUserDefaults().integerForKey("Second"))
-        pauseButton.enabled = false
-        stopButton.enabled = false
+        firstButton!.title = NSString(format: "%d분", NSUserDefaults.standardUserDefaults().integerForKey("First"))
+        secondButton!.title = NSString(format: "%d분", NSUserDefaults.standardUserDefaults().integerForKey("Second"))
+        pauseButton!.enabled = false
+        stopButton!.enabled = false
         defaultAction()
         formatter.dateFormat = "H:mm"
     }
@@ -90,9 +90,9 @@ class TimerController: NSWindowController, TimerDelegate {
 
     @IBAction func start(sender : AnyObject) {
         if timer.start() {
-            startButton.enabled = false
-            pauseButton.enabled = true
-            stopButton.enabled = true
+            startButton!.enabled = false
+            pauseButton!.enabled = true
+            stopButton!.enabled = true
         } else {
             if 0 == timer.remain {
                 defaultAction()
@@ -109,9 +109,9 @@ class TimerController: NSWindowController, TimerDelegate {
 
     @IBAction func pause(sender : AnyObject) {
         if timer.tick {
-            pauseButton.title = "계속"
+            pauseButton!.title = "계속"
         } else {
-            pauseButton.title = "일시정지"
+            pauseButton!.title = "일시정지"
         }
         timer.pause()
     }
